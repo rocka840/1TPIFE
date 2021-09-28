78104191954841c261805dd70632775fc0140cad
Katherine <47138336+rocka840@users.noreply.github.com>
2021-09-28 11:49:31 +0200

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,5 +1,9 @@
 function BLABLA() {
     document.getElementById("hello").innerHTML = "Hello World";
-    document.getElementById("anotherHello").innerHTML = "Hello World";
+    document.getElementById("anotherHello").addEventListener("click",Boop);
+}
+
+function Boop(){
+    alert("You Clicked");
 }
 
